489995b8b1d0a1c0e564c873d51d46cae974f96a
Aman Aset <amanaset12703@gmail.com>
2017-11-21 19:09:10 +0000

    new changes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,30 @@
 class PagesController < ApplicationController
   def home
   end
+  
+  def matte
+    @questions = Question.all.where(category:"Matte")
+  end
+  
+  def naturfag
+    @questions = Question.all.where(category:"Naturfag")
+  end
+  
+  def engelsk
+    @questions = Question.all.where(category:"Engelsk")
+  end
+
+  def samfunnsfag
+    @questions = Question.all.where(category:"Samfunnsfag")
+  end
+  
+  def rle
+    @questions = Question.all.where(category:"RLE")
+  end
+  
+  def norsk
+    @questions = Question.all.where(category:"Norsk")
+  end
+  
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
   resources :questions do
     resources :answers
+    
   end
+  get "k/matte"=>"pages#matte"
+  get "k/naturfag"=>"pages#naturfag"
+  get "k/engelsk"=>"pages#engelsk"
+  get "k/samfunnsfag"=>"pages#samfunnsfag"
+  get "k/rle"=>"pages#rle"
+  get "k/norsk"=>"pages#norsk"
+  
   devise_for :users
   root 'pages#home'
 
