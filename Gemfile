858a1af64898bc3ce9a99db98a1a7a1a69e85a82
Aman Aset <amanaset12703@gmail.com>
2017-11-30 19:06:54 +0000

    new changes

--- Gemfile
+++ Gemfile
@@ -41,3 +41,6 @@ group :production do
 end
 
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+
+gem 'trix'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,4 +2,5 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require turbolinks
+//= require trix
 //= require_tree .
